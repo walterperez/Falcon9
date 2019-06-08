5c8dbe2e62880a091caaa663ee2991c2634832f4
walterperez <waltermaniaco@gmail.com>
2019-06-08 12:36:16 +0100

    Updates

---
+++ precache-manifest.ae368a7a840dcfe39229feaf9b486dbb.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4f04f2ae2bf1f661fd3d2a6f9f08af6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cecfcc5728827ae1b966",
+    "url": "/static/css/main.95b29d05.chunk.css"
+  },
+  {
+    "revision": "043edab3d0268807a9c5",
+    "url": "/static/js/2.1bbde210.chunk.js"
+  },
+  {
+    "revision": "cecfcc5728827ae1b966",
+    "url": "/static/js/main.2873ea13.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "dfda516823176eb19c51d37e805bb515",
+    "url": "/static/media/dragon-graphic3.dfda5168.png"
+  },
+  {
+    "revision": "759a7873869836081f11a456a5ba15c9",
+    "url": "/static/media/fairing-graphic.759a7873.png"
+  },
+  {
+    "revision": "be50af34c1b93d0deaff9b72b0af7c3f",
+    "url": "/static/media/falcon9-render_same_size_file.be50af34.png"
+  },
+  {
+    "revision": "e5ba4cc2f7acb7cf6c1e890f5659b4b5",
+    "url": "/static/media/header___falcon 9.e5ba4cc2.jpg"
+  }
+]);
